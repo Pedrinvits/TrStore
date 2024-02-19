be89dd4428048e721f6f347ebad40cfab9594830
Pedro Mantovani <pedro.mantovani@contabilivre.com.br>
2024-02-19 09:36:31 -0300

    fix : text in saleCreate blade

--- resources/views/SalesViews/saleCreate.blade.php
+++ resources/views/SalesViews/saleCreate.blade.php
@@ -16,7 +16,7 @@
         <input required type="number" min="0.00" max="10000.00" step="0.01" class="form-control" id="sale_value" name="sale_value" placeholder="R$">
     </div>
     <div class="mb-3">
-        <label for="sale_date" class="form-label purple-color">Valor da Venda</label>
+        <label for="sale_date" class="form-label purple-color">Data da Venda</label>
         <input required type="date" class="form-control" id="sale_date" name="sale_date">
     </div>
     <button type="submit" class="btn btn-primary">Criar</button>
